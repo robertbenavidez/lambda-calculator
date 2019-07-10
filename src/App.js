32199454042aeeeae23b303ceec6425b8324d915
Robert Benavidez <murabitbenavidez@gmail.com>
2019-07-10 01:49:26 -0400

    display renders. Added initial styling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import "./App.css";
 
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
 import Operators from './components/ButtonComponents/OperatorButtons/Operators';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials';
+import Display from "./components/DisplayComponents/Display";
+
+
 
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
-import OperatorButton from "./components/ButtonComponents/OperatorButtons/OperatorButton";
+
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -23,6 +27,8 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
+        <Specials />
         <Numbers />
         <Operators />
       </div>
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return <div>{/* Display any props data here */} <h2>0</h2></div>;
 };
+
+
+export default Display; 
